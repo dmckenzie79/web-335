eafcc722767ba0ba30031eddb255cb2e11b41464
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-06-17 21:06:01 -0400

    adding

---
+++ week-8/mckenzie_hello_world.py
@@ -0,0 +1,8 @@
+# ============================================
+# Title: Exercise 8.2
+# Author: Diandra McKenzie
+# Date: 15 June 2020
+# Description: Python file prints Hello World 
+# ============================================
+
+print('Hello World')
