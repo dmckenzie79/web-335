d657d8066fa9fd627b076a8a932713a95d10b5f8
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-06-19 20:55:33 -0400

    adding

---
+++ week-8/mckenzie_calculator.py
@@ -0,0 +1,22 @@
+# ============================================
+# Title: Exercise 8.3
+# Author: Diandra McKenzie
+# Date: 17 June 2020
+# Description: Python in action 
+# add, subtract and divide functions 
+# ============================================
+
+# add function
+def add (x, y):
+    return x + y
+print (add(1, 2))
+
+# subtract function
+def subtract (x, y):
+    return x - y
+print (subtract(4, 1))
+
+# divide function
+def divide (x, y):
+    return x / y
+print (divide(8, 2))
